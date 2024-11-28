90962b21918e3e7d043300a47db6ed5cb0cd4601
issam liqali <63265720+travolta11@users.noreply.github.com>
2024-11-28 11:37:31 +0100

    atelier 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'produits_list.dart';
 
 void main() {
   runApp(const MainApp());
+  
+  Firebase.initializeApp();
+ 
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
